2fcb6c4ede2a65c42799a4f8f17c27c7fa7faa37
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-08-27 06:04:43 +0000

    Sun, Aug 27, 2023, 11:34 AM +05:30

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,2 +1,7 @@
 a = input("File name: ")
-name, ext = a.split(".")
+name, ext = a.lower.split(".")
+match ext:
+    case "jpg" | "jpeg":
+        print("image/jpeg")
+    case "gif":
+        print("image/gif")
