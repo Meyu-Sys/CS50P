bb300961cedff175eedeb480a0a84680272a6d86
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-08-27 07:26:52 +0000

    Sun, Aug 27, 2023, 12:56 PM +05:30

--- meal/meal.py
+++ meal/meal.py
@@ -1,8 +1,17 @@
 def main():
     a = input("What time is it? ")
-    t = convert(a)
+    t = float(convert(a))
+
+    if 7.0 <= t <= 8.0:
+        print("breakfast time")
+    elif 12.0 <= t <= 13.0:
+        print("lunch time")
+    elif 18.0 <= t <= 19.0:
+        print("dinner time")
 
 def convert(time):
-    
+    hr, min = time.split(":")
+    fl = float(float(hr) + float(int(min) / 60))
+    return fl
 
 main()
