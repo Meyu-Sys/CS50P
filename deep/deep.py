a3561c7c9221e4dbceff68a74fcfc28103c7eefd
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-08-27 04:45:50 +0000

    Sun, Aug 27, 2023, 10:15 AM +05:30

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,5 @@
 a = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
-if int(a) == 42 or a == "forty-two" or a == "forty two":
+if a == "forty-two" or a == "forty two" or int(a) == 42:
     print("Yes")
 else:
     print("No")
