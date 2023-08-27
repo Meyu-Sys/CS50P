d3d1603c3d4c429e3625844f2d085d5df0e81dc5
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-08-27 05:45:30 +0000

    Sun, Aug 27, 2023, 11:15 AM +05:30

--- bank/bank.py
+++ bank/bank.py
@@ -5,4 +5,10 @@ def main():
 
 def dollars(g):
     if g[0:5] == "Hello":
-        return
+        return "$0"
+    elif g[0:1] == "H":
+        return "$20"
+    else:
+        return "$100"
+
+main()
