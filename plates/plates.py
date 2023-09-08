f4fca5a15d9b90468d3af31e669406123de322ca
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-09-08 12:32:02 +0000

    Fri, Sep 8, 2023, 6:02 PM +05:30

--- plates/plates.py
+++ plates/plates.py
@@ -9,10 +9,12 @@ def main():
 def is_valid(s):
     if s.length() > 6 or s.length() < 2:
         return False
-    if s[0:2].isalpha == True:
+    if s[0:2].isalpha() == True:
         return False
     for c in s:
-        
+        if c.isalnum() == False:
+            return False
+    for _ in s
 
 
 main()
