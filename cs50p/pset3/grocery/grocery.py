9c56f268db393fdf88ac4a0014dbd51cc6895252
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-09-17 17:32:59 +0000

    Sun, Sep 17, 2023, 11:02 PM +05:30

--- cs50p/pset3/grocery/grocery.py
+++ cs50p/pset3/grocery/grocery.py
@@ -1,4 +1,4 @@
-list = {
+listicle = {
 
 }
 sortlist = {
@@ -7,14 +7,11 @@ sortlist = {
 while True:
     try:
         a = input("").upper()
-        if a in list:
-            list[a] = int(list[a]) + 1
+        if a in listicle:
+            listicle[a] = int(listicle[a]) + 1
         else:
-            list[a] = 1
+            listicle[a] = 1
     except EOFError:
-        keysme = list(list.keys())
-        keysme.sort()
-        for i in keys:
-            sortlist[i] = list[i]
+        sortlist = dict(sorted(listicle.items()))
         for items in sortlist:
-            print(list[items], items)
+            print(sortlist[items], items)
