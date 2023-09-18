a5dbb86326709a2ea9754f40a91cabd174670017
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-09-18 16:44:46 +0000

    Mon, Sep 18, 2023, 10:14 PM +05:30

--- cs50p/pset3/outdated/outdated.py
+++ cs50p/pset3/outdated/outdated.py
@@ -12,22 +12,23 @@ months = [
     "November",
     "December",
 ]
-date = input("Date: ")
-if "/" in date:
-    x, y, z = date.split("/")
-    x = int(x)
-    y = int(y)
-    z = int(z)
-    print(f"{z}-{x:02d}-{y:02d}")
-else:
-    x, y, z = date.split(" ")
-    y = int(y.replace(",",""))
-    z = int(z)
-    for n in months:
-        if x == months[n]:
-            x = int(n) + int(1)
-            break
-        else:
-            continue
-    x = int(x)
-    print(f"{z}-{x:02d}-{y:02d}")
+while True:
+    date = input("Date: ")
+    if "/" in date:
+        x, y, z = date.split("/")
+        x = int(x)
+        y = int(y)
+        z = int(z)
+        print(f"{z}-{x:02d}-{y:02d}")
+    else:
+        x, y, z = date.split(" ")
+        y = int(y.replace(",",""))
+        z = int(z)
+        for n in months:
+            if x == months[n]:
+                x = int(n) + int(1)
+                break
+            else:
+                continue
+        x = int(x)
+        print(f"{z}-{x:02d}-{y:02d}")
