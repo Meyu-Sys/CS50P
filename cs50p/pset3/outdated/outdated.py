11bc5837274cb3b8cbfb5fa35c2b36cc5484b2f2
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-09-18 16:29:43 +0000

    Mon, Sep 18, 2023, 9:59 PM +05:30

--- cs50p/pset3/outdated/outdated.py
+++ cs50p/pset3/outdated/outdated.py
@@ -1,6 +1,11 @@
 date = input()
 if "/" in date:
     x, y, z = date.split("/")
-    print(f"{z}-{x:02}-{y:02}")
+    x = int(x)
+    y = int(y)
+    z = int(z)
+    print(f"{z}-{x:02d}-{y:02d}")
 else:
     x, y, z = date.split(" ")
+    y = int(y - ",")
+    z = int(z)
