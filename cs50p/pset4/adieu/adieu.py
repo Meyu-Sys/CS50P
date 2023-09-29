4d37cf213b22e352b8c7a3ab54160caaf2207bdc
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-09-29 09:57:22 +0000

    Fri, Sep 29, 2023, 3:27 PM +05:30

--- cs50p/pset4/adieu/adieu.py
+++ cs50p/pset4/adieu/adieu.py
@@ -1,9 +1,9 @@
 names = []
 count = 0
-printstr = "Adieu, adieu, to"
+printstr = "Adieu, adieu, to "
 try:
     while True:
         names[count] = input("Name: ")
         count = count + 1
 except EOFError:
-    
+    printstr = printstr + 
