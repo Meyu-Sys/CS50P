64e51f774304eac17b6b73d1a178c927d2eab82c
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-09-29 08:44:58 +0000

    Fri, Sep 29, 2023, 2:14 PM +05:30

--- cs50p/pset4/figlet/figlet.py
+++ cs50p/pset4/figlet/figlet.py
@@ -2,13 +2,15 @@ import sys
 import pyfiglet
 import random
 
+fonts = []
+
 if len(sys.argv) == 3:
     if sys.argv[1] == "-f" or sys.argv[1] == "--font":
         fon = sys.argv[2]
     else:
         sys.exit("Invalid usage")
 elif len(sys.argv) == 1:
-    
+
 else:
     sys.exit("Invalid usage")
 
