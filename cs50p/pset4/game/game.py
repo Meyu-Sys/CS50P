fb408d4f284519612b7ae5f0ed91e628dd89582c
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-09-29 10:30:56 +0000

    Fri, Sep 29, 2023, 4:00 PM +05:30

--- cs50p/pset4/game/game.py
+++ cs50p/pset4/game/game.py
@@ -6,3 +6,14 @@ while True:
     else:
         break
 rand = random.randint(1,up)
+while True:
+    guess = int(input("Guess: "))
+    if guess < rand:
+        print("Too small!")
+        continue
+    elif guess > rand:
+        print("Too large!")
+        continue
+    else:
+        print("Just right!")
+        exit
