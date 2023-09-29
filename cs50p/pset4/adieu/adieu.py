6614ef40b4c705c7598cba51fc353604bfc2d53e
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-09-29 09:51:36 +0000

    Fri, Sep 29, 2023, 3:21 PM +05:30

--- cs50p/pset4/adieu/adieu.py
+++ cs50p/pset4/adieu/adieu.py
@@ -7,4 +7,7 @@ try:
         count = count + 1
 except EOFError:
     for name in names:
-        if count >
+        if count > 1:
+            printstr = printstr + " " + name + ","
+        elif count == 1:
+            printstr = printstr + " and " + name
