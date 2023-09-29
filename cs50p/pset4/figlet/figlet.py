e4abab49ff35c87666b3a46e686cd6b8f44fad4d
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-09-29 09:15:09 +0000

    Fri, Sep 29, 2023, 2:45 PM +05:30

--- cs50p/pset4/figlet/figlet.py
+++ cs50p/pset4/figlet/figlet.py
@@ -20,7 +20,10 @@ fonts = [
 
 if len(sys.argv) == 3:
     if sys.argv[1] == "-f" or sys.argv[1] == "--font":
-        if 
+        if sys.argv[2] in fonts:
+            fon = sys.argv[2]
+        else:
+            sys.exit("Invalid usage")
     else:
         sys.exit("Invalid usage")
 elif len(sys.argv) == 1:
