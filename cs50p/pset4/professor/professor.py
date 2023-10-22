5bdbe35d487187f7ebbe8e7eb7cdd9e52a6e8e24
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-10-22 14:36:17 +0000

    Sun, Oct 22, 2023, 8:06 PM +05:30

--- cs50p/pset4/professor/professor.py
+++ cs50p/pset4/professor/professor.py
@@ -43,7 +43,10 @@ def main():
 
 def get_level():
     while True:
-        n = int(input("Level: "))
+        try:
+            n = int(input("Level: "))
+        except ValueError:
+            continue
         if n == 1 or n == 2 or n == 3:
             return n
         else:
