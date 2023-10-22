acb09b1c8eb6f8fa258e65d6fa6329d19000dcc8
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-10-22 16:23:16 +0000

    Sun, Oct 22, 2023, 9:53 PM +05:30

--- cs50p/pset4/bitcoin/bitcoin.py
+++ cs50p/pset4/bitcoin/bitcoin.py
@@ -10,3 +10,5 @@ resp = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 rate = ((resp.json()["bpi"])["USD"])["rate"]
 rt1,rt2 = rate.split(",")
 rate = float(rt1 + rt2)
+price = n * rate
+print(f"${price:,.4f}")
