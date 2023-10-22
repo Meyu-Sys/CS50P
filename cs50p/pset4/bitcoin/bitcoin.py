e78dd1b8b02f04fb4614a47c9165205ac10ef11b
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-10-22 16:07:31 +0000

    Sun, Oct 22, 2023, 9:37 PM +05:30

--- cs50p/pset4/bitcoin/bitcoin.py
+++ cs50p/pset4/bitcoin/bitcoin.py
@@ -1,4 +1,4 @@
 import requests
 import json
 resp = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-print(json.dump(resp.json(), indent=2))
+print(json.dumps(resp.json(), indent=2))
