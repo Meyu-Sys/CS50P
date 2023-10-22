77cb553d8a1805b7ce81fe9f0fe62e4bcf019ac4
Meyu-Sys <86107480+Meyu-Sys@users.noreply.github.com>
2023-10-22 15:25:05 +0000

    Sun, Oct 22, 2023, 8:55 PM +05:30

--- cs50p/pset4/professor/professor.py
+++ cs50p/pset4/professor/professor.py
@@ -8,14 +8,14 @@ def main():
     Score = 0
     while n < 10:
         if diff == 1:
-            X = random.randint(0,9)
-            Y = random.randint(0,9)
+            X = random.randint(0, 9)
+            Y = random.randint(0, 9)
         elif diff == 2:
-            X = random.randint(10,99)
-            Y = random.randint(10,99)
+            X = random.randint(10, 99)
+            Y = random.randint(10, 99)
         else:
-            X = random.randint(100,999)
-            Y = random.randint(100,999)
+            X = random.randint(100, 999)
+            Y = random.randint(100, 999)
         Z = X + Y
         pr = str(X) + " + " + str(Y) + " = "
         prans = pr + str(Z)
@@ -41,6 +41,7 @@ def main():
         n = n + 1
     print("Score:", Score)
 
+
 def get_level():
     while True:
         try:
@@ -52,16 +53,18 @@ def get_level():
         else:
             continue
 
+
 def generate_integer(level):
     if level == 1:
-        o = random.randint(0,9)
+        o = random.randint(0, 9)
     elif level == 2:
-        o = random.randint(10,99)
+        o = random.randint(10, 99)
     elif level == 3:
-        o = random.randint(100,999)
+        o = random.randint(100, 999)
     else:
         raise ValueError("Level not Valid")
     return o
+
 
 if __name__ == "__main__":
     main()
